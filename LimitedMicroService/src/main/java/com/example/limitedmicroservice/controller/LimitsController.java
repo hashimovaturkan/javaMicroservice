9d1ecfb7f3d16d4e1ef68aa90def95656735b0fa
fhashimovaa <fidanhewimova@gmail.com>
2024-01-20 13:38:30 +0400

    init

---
+++ LimitedMicroService/src/main/java/com/example/limitedmicroservice/controller/LimitsController.java
@@ -0,0 +1,24 @@
+package com.example.limitedmicroservice.controller;
+
+import com.example.limitedmicroservice.bean.Limits;
+import com.example.limitedmicroservice.configuration.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class LimitsController {
+
+    private Configuration configuration;
+
+
+    public LimitsController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    @GetMapping("/limits")
+    public Limits getLimits(){
+        return new Limits(configuration.getMinimum(),configuration.getMinimum());
+    }
+}
