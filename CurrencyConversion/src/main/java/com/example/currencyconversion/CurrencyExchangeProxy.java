7b75139efb6beebf14f4b5b6b02182bb087ff0b5
Turkan Hashimova <turkan.hashimova@azintelecom.az>
2024-01-22 15:20:43 +0400

    test

--- CurrencyConversion/src/main/java/com/example/currencyconversion/CurrencyExchangeProxy.java
+++ CurrencyConversion/src/main/java/com/example/currencyconversion/CurrencyExchangeProxy.java
@@ -4,11 +4,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.math.BigDecimal;
+
 //Feign projesinin amacı java http clientlarını kolayca oluşturmayı sağlamaktır.
 //diger servicellere requestleri burdan atiriq
-@FeignClient(name = "currency-exchange",url = "http://localhost:8000")
+//@FeignClient(name = "currency-exchange",url = "http://localhost:8000")
+@FeignClient(name = "currency-exchange")  //artiq naming server(service registry) vasitelise yuxardaki kimi yox bele yaza bilerik, eureka ile danisir bilir urlini yerini filan
 public interface CurrencyExchangeProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
 }
+//naming server portlarin falan meselen currencyExchangeproxy de hard code (http://localhost:8000) yazmadan briyerden idare etmek ucundu
